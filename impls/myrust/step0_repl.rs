0b9167902638480d8a1b1eb3d82695692d4bcd90
Timofei Likulin <tlikulin@gmail.com>
2026-06-08 17:15:36 +0200

    step0

--- impls/myrust/step0_repl.rs
+++ impls/myrust/step0_repl.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("works");
+use std::io::{self, Write};
+
+fn main() -> io::Result<()> {
+    loop {
+        print!("user> ");
+        io::stdout().flush()?;
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input)?;
+
+        if input.is_empty() {
+            return Ok(());
+        } else {
+            let output = rep(input);
+            print!("{output}");
+        }
+    }
+}
+
+fn read(input: String) -> String {
+    input
+}
+
+fn eval(input: String) -> String {
+    input
+}
+
+fn print(input: String) -> String {
+    input
+}
+
+fn rep(input: String) -> String {
+    print(eval(read(input)))
 }
